d222e5533c0d08b5a68928d01b4614a8db374903
Moisés Mariano <moisesduartem@gmail.com>
2020-10-25 13:09:18 -0300

    receives the actual uri, the baseUrl & register the diff of them

--- bootstrap/UrlHandler.php
+++ bootstrap/UrlHandler.php
@@ -3,8 +3,82 @@
 
 namespace Bootstrap
 {
+    /**
+     * Class UrlHandler
+     * @package Bootstrap
+     */
     class UrlHandler
     {
+        /**
+         * @var string
+         */
+        protected string $baseUrl;
+        /**
+         * @var string
+         */
+        protected string $actualUrl;
+        /**
+         * @var string
+         */
+        protected string $actualRoute;
 
+        /**
+         * UrlHandler constructor.
+         * @param string $baseUrl
+         */
+        public function __construct(string $baseUrl)
+        {
+            $this->setBaseUrl($baseUrl);
+            $this->setActualUrl($_SERVER['REQUEST_URI']);
+            $this->setActualRoute(str_replace($this->getBaseUrl(), '', $this->getActualUrl()));
+        }
+
+        /**
+         * @param string $baseUrl
+         */
+        public function setBaseUrl(string $baseUrl): void
+        {
+            $this->baseUrl = $baseUrl;
+        }
+
+        /**
+         * @return string
+         */
+        public function getBaseUrl(): string
+        {
+            return $this->baseUrl;
+        }
+
+        /**
+         * @param mixed|string $actualUrl
+         */
+        public function setActualUrl($actualUrl): void
+        {
+            $this->actualUrl = $actualUrl;
+        }
+
+        /**
+         * @return mixed|string
+         */
+        public function getActualUrl()
+        {
+            return $this->actualUrl;
+        }
+
+        /**
+         * @param string $actualRoute
+         */
+        public function setActualRoute(string $actualRoute): void
+        {
+            $this->actualRoute = $actualRoute;
+        }
+
+        /**
+         * @return string
+         */
+        public function getActualRoute(): string
+        {
+            return $this->actualRoute;
+        }
     }
 }
