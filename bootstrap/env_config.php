1c4e93dbdb4f4ebfcc6ac79c8bc05db8cb59b5d9
Moisés Mariano <moisesduartem@gmail.com>
2020-10-26 00:25:32 -0300

    define database credentials

--- bootstrap/env_config.php
+++ bootstrap/env_config.php
@@ -1,4 +1,13 @@
 <?php
 
+/**
+ * .env file definitions.
+ */
+
 $env = parse_ini_file(__DIR__ . '/../.env');
 define('BASE_URL', $env['BASE_URL']);
+define('SGBD', $env['SGBD']);
+define('HOST', $env['HOST']);
+define('DB_NAME', $env['DB_NAME']);
+define('USERNAME', $env['USERNAME']);
+define('PASSWORD', $env['PASSWORD']);
