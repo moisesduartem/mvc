071e403b9b51d47889d78487997afa0b7ea87bd2
Moisés Mariano <moisesduartem@gmail.com>
2020-10-26 00:26:14 -0300

    ::list() shows users with Database::query()

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Controllers;
+use Bootstrap\Database;
 
 class IndexController
 {
     public function index()
     {
        echo "Eureka!";
+    }
+
+    public function list()
+    {
+        $stmt = Database::query('select * from users where name = :name', ['name' => 'Moisés']);
+        print_r($stmt->fetchAll(\PDO::FETCH_ASSOC));
     }
 }
