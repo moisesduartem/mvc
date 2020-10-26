3c65ba1f837d63cc2265a8d0ada207f0cea0b04b
Moisés Mariano <moisesduartem@gmail.com>
2020-10-26 00:26:37 -0300

    create route IndexController.list

--- routes/routes.php
+++ routes/routes.php
@@ -4,5 +4,6 @@ $router = new Router(BASE_URL);
 $router->setNamespace('App\\Controllers\\');
 
 $router->on('index', 'IndexController.index');
+$router->on('', 'IndexController.list');
 
 $router->run();
